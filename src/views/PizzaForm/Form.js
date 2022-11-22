69186b3b47f1997b758bc582358559223951f5b0
buraiyen <brian.edison.nguyen@gmail.com>
2022-11-21 17:18:55 -0800

    feat: more topping options

--- src/views/PizzaForm/Form.js
+++ src/views/PizzaForm/Form.js
@@ -4,81 +4,85 @@ const Form = () => {
     console.log('Submitted!');
   };
   return (
-    <form id='pizza-form' onSubmit={submitHandler}>
-      <section className='form__container'>
-        <h2 className='form__header'>Build Your Own Pizza</h2>
+    <form id="pizza-form" onSubmit={submitHandler}>
+      <section className="form__container">
+        <h2 className="form__header">Build Your Own Pizza</h2>
         {/* Pizza size */}
-        <label htmlFor='size'>
+        <label htmlFor="size">
           <h3>Choice of size</h3>
           <p>Required *</p>
         </label>
-        <select name='size' id=''>
-          <option value='small'>Small</option>
-          <option value='medium'>Medium</option>
-          <option value='large'>Large</option>
+        <select name="size" id="">
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
         </select>
 
         {/* Pizza sauce */}
-        <label htmlFor='sauce'>
+        <label htmlFor="sauce">
           <h3>Choice of Sauce</h3>
           <p>Required *</p>
         </label>
-        <input type='radio' name='sauce' value='original_red' />
-        <label htmlFor='sauce'>Original Red</label>
+        <input type="radio" name="sauce" value="original_red" />
+        <label htmlFor="sauce">Original Red</label>
         <br />
-        <input type='radio' name='sauce' value='garlic_ranch' />
-        <label htmlFor='sauce'>Garlic Ranch</label>
+        <input type="radio" name="sauce" value="garlic_ranch" />
+        <label htmlFor="sauce">Garlic Ranch</label>
         <br />
-        <input type='radio' name='sauce' value='bbq_sauce' />
-        <label htmlFor='sauce'>BBQ Sauce</label>
+        <input type="radio" name="sauce" value="bbq_sauce" />
+        <label htmlFor="sauce">BBQ Sauce</label>
         <br />
-        <input type='radio' name='sauce' value='spinach_alfredo' />
-        <label htmlFor='sauce'>Spinach Alfredo</label>
+        <input type="radio" name="sauce" value="spinach_alfredo" />
+        <label htmlFor="sauce">Spinach Alfredo</label>
 
         {/* Pizza toppings */}
-        <label htmlFor='toppings'>
+        <label htmlFor="toppings">
           <h3>Add toppings</h3>
           <p>Choose up to 10</p>
         </label>
-        <div className='form__container--toppings'>
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='pepperoni' />
+        <div className="form__container--toppings">
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="pepperoni" />
             Pepperoni
           </label>
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='diced_tomatoes' />
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="diced_tomatoes" />
             Diced Tomatoes
           </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='sausage' />
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="sausage" />
             Sausage
           </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='black_olives' />
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="black_olives" />
             Black Olives
           </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='' />
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="canadian_bacon" />
+            Canadian Bacon
+          </label>{' '}
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="roasted_garlic" />
+            Roasted Garlic
+          </label>{' '}
+          <label htmlFor="toppings" className="topping">
+            <input
+              type="checkbox"
+              name="toppings"
+              value="spicy_italian_sausage"
+            />
+            Spicy Italian Sausage
+          </label>{' '}
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="artichoke_hearts" />
+            Artichoke Hearts
+          </label>{' '}
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="pepperoni" />
             Pepperoni
           </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='pepperoni' />
-            Pepperoni
-          </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='pepperoni' />
-            Pepperoni
-          </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='pepperoni' />
-            Pepperoni
-          </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='pepperoni' />
-            Pepperoni
-          </label>{' '}
-          <label htmlFor='toppings' className='topping'>
-            <input type='checkbox' name='toppings' value='pepperoni' />
+          <label htmlFor="toppings" className="topping">
+            <input type="checkbox" name="toppings" value="pepperoni" />
             Pepperoni
           </label>
         </div>
