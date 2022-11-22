de18a895a46a436317e5f00bc5d2953d2e26b5bc
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-11-22 10:53:26 -0800

    init formData state and input change handler

--- src/views/PizzaForm/Form.js
+++ src/views/PizzaForm/Form.js
@@ -3,101 +3,113 @@ import './Form.css';
 import Button from '../../components/ui/Button';
 const Form = () => {
   // States
-
+  const [formData, setFormData] = useState({
+    fullName: '',
+  });
   // Handlers
   const submitHandler = (event) => {
     event.preventDefault();
     alert('Submitted!');
   };
+
+  const inputChangeHandler = (event) => {
+    const { type, name, checked, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
-    <form id="pizza-form" onSubmit={submitHandler}>
-      <section className="form__container">
-        <h2 className="form__header">Build Your Own Pizza</h2>
+    <form id='pizza-form' onSubmit={submitHandler}>
+      <section className='form__container'>
+        <h2 className='form__header'>Build Your Own Pizza</h2>
         <section>
-          <label htmlFor="full_name">
+          <label htmlFor='fullName'>
             <h3>Name</h3>
           </label>
-          <input type="text" name="full_name" id="name-input" />
+          <input
+            type='text'
+            name='fullName'
+            id='name-input'
+            onChange={inputChangeHandler}
+          />
         </section>
         {/* Pizza size */}
         <section>
-          <label htmlFor="size">
+          <label htmlFor='size'>
             <h3>Choice of size</h3>
             <p>Required *</p>
           </label>
-          <select name="size" id="">
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
+          <select name='size' id=''>
+            <option value='small'>Small</option>
+            <option value='medium'>Medium</option>
+            <option value='large'>Large</option>
           </select>
         </section>
         {/* Pizza sauce */}
         <section>
-          <label htmlFor="sauce">
+          <label htmlFor='sauce'>
             <h3>Choice of Sauce</h3>
             <p>Required *</p>
           </label>
-          <input type="radio" name="sauce" value="original_red" />
-          <label htmlFor="sauce">Original Red</label>
+          <input type='radio' name='sauce' value='original_red' />
+          <label htmlFor='sauce'>Original Red</label>
           <br />
-          <input type="radio" name="sauce" value="garlic_ranch" />
-          <label htmlFor="sauce">Garlic Ranch</label>
+          <input type='radio' name='sauce' value='garlic_ranch' />
+          <label htmlFor='sauce'>Garlic Ranch</label>
           <br />
-          <input type="radio" name="sauce" value="bbq_sauce" />
-          <label htmlFor="sauce">BBQ Sauce</label>
+          <input type='radio' name='sauce' value='bbq_sauce' />
+          <label htmlFor='sauce'>BBQ Sauce</label>
           <br />
-          <input type="radio" name="sauce" value="spinach_alfredo" />
-          <label htmlFor="sauce">Spinach Alfredo</label>
+          <input type='radio' name='sauce' value='spinach_alfredo' />
+          <label htmlFor='sauce'>Spinach Alfredo</label>
         </section>
         {/* Pizza toppings */}
         <section>
-          <label htmlFor="toppings">
+          <label htmlFor='toppings'>
             <h3>Add toppings</h3>
             <p>Choose up to 10</p>
           </label>
-          <div className="form__container--toppings">
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="pepperoni" />
+          <div className='form__container--toppings'>
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='pepperoni' />
               Pepperoni
             </label>
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="diced_tomatoes" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='diced_tomatoes' />
               Diced Tomatoes
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="sausage" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='sausage' />
               Sausage
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="black_olives" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='black_olives' />
               Black Olives
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="canadian_bacon" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='canadian_bacon' />
               Canadian Bacon
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="roasted_garlic" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='roasted_garlic' />
               Roasted Garlic
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
+            <label htmlFor='toppings' className='topping'>
               <input
-                type="checkbox"
-                name="toppings"
-                value="spicy_italian_sausage"
+                type='checkbox'
+                name='toppings'
+                value='spicy_italian_sausage'
               />
               Spicy Italian Sausage
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="artichoke_hearts" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='artichoke_hearts' />
               Artichoke Hearts
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="vegan_chicken" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='vegan_chicken' />
               Vegan Chicken
             </label>{' '}
-            <label htmlFor="toppings" className="topping">
-              <input type="checkbox" name="toppings" value="tofu" />
+            <label htmlFor='toppings' className='topping'>
+              <input type='checkbox' name='toppings' value='tofu' />
               Tofu
             </label>
           </div>
@@ -106,20 +118,20 @@ const Form = () => {
         <section>
           <h3>Choice of Substitute</h3>
           <p>Choose up to one</p>
-          <input type="checkbox" name="substitute" />
-          <label htmlFor="substitute">Gluten Free Crust (+ $1.00)</label>
+          <input type='checkbox' name='substitute' />
+          <label htmlFor='substitute'>Gluten Free Crust (+ $1.00)</label>
         </section>
         <section>
           <h3>Special Instructions</h3>
           <input
-            type="text"
+            type='text'
             placeholder="Anything else you'd like to have?"
-            className="form__instructions"
+            className='form__instructions'
           />
         </section>
         <section style={{ marginTop: '10px' }}>
-          <input type="number" />
-          <Button type="primary">Add to Order</Button>
+          <input type='number' />
+          <Button type='primary'>Add to Order</Button>
         </section>
       </section>
     </form>
